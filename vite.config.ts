62e222fa38840be19c601189e3436e428e69ac9e
NikitaNekhay <ktofreesapiens@gmail.com>
2026-04-15 11:19:03 +0300

    Fix deployment issue with npm

--- vite.config.ts
+++ vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      port: 3000,
+      port: 5173,
       proxy: {
         '/api/v1': {
           target: env.VITE_API_BASE_URL || 'http://localhost:8080',
